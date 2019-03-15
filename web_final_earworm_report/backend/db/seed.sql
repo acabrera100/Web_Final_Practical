14ba5d974bf3d38a176dff30d0bc98bdfbe71ca5
Andres.C <andrescabrera@pursuit.org>
2019-03-15 13:01:52 -0400

    fake data added

--- web_final_earworm_report/backend/db/seed.sql
+++ web_final_earworm_report/backend/db/seed.sql
@@ -23,18 +23,28 @@ genre_id INT REFERENCES genres(id)
 );
 
 CREATE TABLE favorites(
-id SERIAL PRIMARY KEY
+id SERIAL PRIMARY KEY,
+user_id INT REFERENCES users(id),
+song_id INT REFERENCES songs(id)
 );
+
 CREATE TABLE comments(
-id SERIAL PRIMARY KEY
+id SERIAL PRIMARY KEY,
+comment_body VARCHAR NOT NULL,
+user_id INT REFERENCES users(id),
+song_id INT REFERENCES songs(id)
 );
 
 INSERT INTO users(username)
 VALUES ('felipe_queens'), ('kevlinMai'), ('squibbles'), ('scooby_dooby420'), ('anonymousBlissXoXo'), ('hippyDippy'), ('Metaru_Gurido_Soriduo'), ('Oscar De Leon'), ('celia_cruzin4bruisin'), ('#U2');
+
 INSERT INTO genres(genre_name)
 VALUES ('salsa'), ('merengue'), ('rap'), ('jazz'), ('rock');
+
 INSERT INTO songs (title ,artist, img_url , user_id , genre_id)
-VALUES ('Lloraras','Oscar De Leon', 'https://m.media-amazon.com/images/I/81D1UcUdESL._SS500_.jpg', 8, 1),
+VALUES
+
+('Lloraras','Oscar De Leon', 'https://m.media-amazon.com/images/I/81D1UcUdESL._SS500_.jpg', 8, 1),
 ('La Vaca','Mala Fe', 'https://images-na.ssl-images-amazon.com/images/I/51FtpLi8pZL.jpg', 9, 2),
 ('N.Y State of Mind','Nas', 'https://i.scdn.co/image/0e1dd10af8d8ce24a4309d1132491286a9bd685d', 1, 3),
 ('Birds Lament','MoonDog', 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c9/Moondog_%281969_Moondog_album%29.jpg/220px-Moondog_%281969_Moondog_album%29.jpg', 6, 4),
@@ -49,3 +59,33 @@ VALUES ('Lloraras','Oscar De Leon', 'https://m.media-amazon.com/images/I/81D1UcU
 ('Merengue Ripiao','Luis Lopez', 'https://m.media-amazon.com/images/I/91em-klGpyL._SS500_.jpg', 1, 2),
 ('All NightMare Long','Metallica', 'https://upload.wikimedia.org/wikipedia/en/thumb/1/1b/Metallica_-_All_Nightmare_Long_cover_1.jpg/220px-Metallica_-_All_Nightmare_Long_cover_1.jpg', 7, 5),
 ('98 Freestyle','Big L ft. Jay-Z', 'http://images.genius.com/c25759cb0f6915163b953fb6a3367d64.900x895x1.jpg', 4, 3);
+
+INSERT INTO favorites(user_id,song_id)
+VALUES (1,4),(1,1),(1,8),(1,15),(2,10),(1,6),(1,13),(2,14),
+(3,3),(3,5),(3,6),(3,9),(3,15),(3,14),(4,4),(4,6),
+(5,1),(5,2),(5,3),(5,4),(5,5),(6,4),(6,10),(7,5),
+(7,15),(7,11),(8,1),(8,13),(8,2),(9,12),(9,8),(9,7),(8,7),
+(10,14),(10,11),(10,5),(5,14),(5,11),(5,9),(1,15),(8,12);
+
+INSERT INTO comments(comment_body,user_id,song_id)
+VALUES
+('This song opened me up to how silly and awesome jazzcan be',1,4),
+('Oldie but goodie, essentials of salsa',1,1),
+('Tyler the Creator really brings the song together',5,9),
+('Oldie but goodie, essentials of salsa',1,1),
+('man you feel like you are there sometimes',6,10),
+('looking for a good time or a pick me up. Play this when you gotta clean the house',8,13),
+('get hype East Coast style',7,15),
+('some of theses names are funny, but cemented as a legendary song still',8,7),
+('lil wayne with some bud and coffee. LIT AF',4,6),
+('when you gotta put the petal to the metal and just surge.No thinking just going',2,14),
+('nostalgia brah',7,5),
+('man I wanna go to DR',1,13),
+('this is my mantra. I am a New York State of Mind. Change my Mind!',3,3),
+('I am the master at this at Guitar Hero. Anyone defies me know where you can find me.',10,11),
+('you have to , have to be from Colombia if you like this song',8,12),
+('brah, ya know brah. Lite one for this old timer. ',6,4),
+('WTH is going on this song. Its pure garbage',10,14),
+('uhm just cuz you think that, its solely your opinion, go be belligerent elsewhere',1,14),
+('yea ok your probably just a 9 yr old who heard it for the first time.GTFO here',10,14),
+('I just exposed your IP address on www.trollerroller.com, might wanna be more careful with your words next time',1,14);
