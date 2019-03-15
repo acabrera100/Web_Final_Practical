fba9899c2ffcaac374299de350c8ccc2a5615a59
Andres.C <andrescabrera@pursuit.org>
2019-03-15 12:08:15 -0400

    Been putting my value fields in seperate parenthesis instead of all in one

--- web_final_earworm_report/backend/db/schema.md
+++ web_final_earworm_report/backend/db/schema.md
@@ -3,18 +3,18 @@ The following tables and columns will be necessary.
 
 Users
 <!-- -id -->
--username - Unique
+<!-- -username - Unique -->
 
 Genres
 <!-- -id -->
--genre_name - Unique
+<!-- -genre_name - Unique -->
 
 Songs
 <!-- -id -->
--title
--img_url
--user_id - References Users
--genre_id - References Genres
+<!-- -title -->
+<!-- -img_url -->
+<!-- -user_id - References Users -->
+<!-- -genre_id - References Genres -->
 
 Favorites
 <!-- -id -->
@@ -27,4 +27,10 @@ Comments
 -user_id - References Users
 -song_id - References Songs
 
-Create an .sql file to create these tables and seed this database with at least 10 users, 5 genres, 15 songs, 40 favorites, and 20 comments. Your first user should be your sample user - the user that we will automatically be logged-in as.
+-Create an .sql file to create these tables and seed this database with at least
+<!-- -10 users,  -->
+<!-- -5 genres, -->
+- 15 songs,
+-40 favorites,
+-and 20 comments.
+-Your first user should be your sample user - the user that we will automatically be logged-in as.
--- web_final_earworm_report/backend/db/seed.sql
+++ web_final_earworm_report/backend/db/seed.sql
@@ -4,17 +4,34 @@ CREATE DATABASE musicstoragedatabase;
 \c musicstoragedatabase;
 
 CREATE TABLE users(
-id SERIAL PRIMARY KEY
+id SERIAL PRIMARY KEY,
+username VARCHAR UNIQUE
 );
+
 CREATE TABLE genres(
-id SERIAL PRIMARY KEY
+id SERIAL PRIMARY KEY,
+genre_name VARCHAR UNIQUE
 );
+
 CREATE TABLE songs(
-id SERIAL PRIMARY KEY
+id SERIAL PRIMARY KEY,
+title VARCHAR NOT NULL,
+artist VARCHAR NOT NULL,
+img_url VARCHAR NOT NULL,
+user_id INT REFERENCES users(id),
+genre_id INT REFERENCES genres(id)
 );
+
 CREATE TABLE favorites(
 id SERIAL PRIMARY KEY
 );
 CREATE TABLE comments(
 id SERIAL PRIMARY KEY
 );
+
+INSERT INTO users(username)
+VALUES ('felipe_queens'), ('kevlinMai'), ('squibbles'), ('scooby_dooby420'), ('anonymousBlissXoXo'), ('hippyDippy'), ('Metaru_Gurido_Soriduo'), ('Oscar De Leon'), ('celia_cruzin4bruisin'), ('#U2');
+INSERT INTO genres(genre_name)
+VALUES ('salsa'), ('merengue'), ('rap'), ('jazz'), ('rock');
+INSERT INTO songs (title ,artist, img_url , user_id , genre_id)
+VALUES ('Lloraras','Oscar De Leon', 'https://m.media-amazon.com/images/I/81D1UcUdESL._SS500_.jpg', 8, 1);
