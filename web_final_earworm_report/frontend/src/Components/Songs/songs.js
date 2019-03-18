9636283d76d63ce070ae5f05e15313143f0d6ece
Andres.C <andrescabrera@pursuit.org>
2019-03-18 19:44:52 -0400

    all songs search done, next to render a single song with username link to /profile/:id

--- web_final_earworm_report/frontend/src/Components/Songs/songs.js
+++ web_final_earworm_report/frontend/src/Components/Songs/songs.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import axios from "axios";
 import SongByPopularity from "./songsByPopularity.js";
 import SongsByGenre from "./songsByGenre.js";
@@ -9,7 +9,8 @@ class Songs extends Component {
   constructor() {
     super();
     this.state = {
-      songs: []
+      songs: [],
+      formInputText: ""
     };
   }
 
@@ -21,45 +22,84 @@ class Songs extends Component {
     });
   }
 
+  handleChange = e => {
+    this.setState({ formInputText: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    let songSearched = this.state.songs.find(song => {
+      return (
+        song.name
+          .toLowerCase()
+          .indexOf(this.state.formInputText.toLowerCase()) === 0
+      );
+    });
+    this.state.history.push(`/songs/${songSearched.id}/singleSong`);
+  };
   render() {
     console.log(this.state);
+
     let songsList = this.state.songs.map(song => {
-      return <li key={song.id}>{song.title}</li>;
+      if (
+        song.title
+          .toLowerCase()
+          .indexOf(this.state.formInputText.toLowerCase()) === 0
+      ) {
+        return (
+          <li key={song.id}>
+            <Link to={"/songs/" + song.id}>{song.title}</Link>
+          </li>
+        );
+      } else {
+        return null;
+      }
     });
     return (
-      <Switch>
-        <Route
-          exact
-          path="/songs"
-          render={() => {
-            return (
-              <>
-                <h1> All Songs</h1>
-                <ul>{songsList}</ul>
-              </>
-            );
-          }}
-        />
-        <Route
-          path="/songs/byPopularity"
-          render={() => {
-            return <SongByPopularity songs={this.state.songs} />;
-          }}
-        />
-        <Route
-          path="/songs/byGenre"
-          render={() => {
-            return <SongsByGenre songs={this.state.songs} />;
-          }}
-        />
-        <Route
-          exact
-          path="/songs/:id"
-          render={() => {
-            return <SingleSong songs={this.state.songs} />;
-          }}
-        />
-      </Switch>
+      <>
+        <Switch>
+          <Route
+            exact
+            path="/songs"
+            render={() => {
+              return (
+                <>
+                  <form onSubmit={this.handleSubmit}>
+                    <input
+                      type="text"
+                      placeholder="Search"
+                      value={this.state.formInputText}
+                      onChange={this.handleChange}
+                    />
+                    <input type="submit" value="Search by Title" />
+                  </form>
+                  <h1> All Songs</h1>
+                  <ul>{songsList}</ul>
+                </>
+              );
+            }}
+          />
+          <Route
+            path="/songs/byPopularity"
+            render={() => {
+              return <SongByPopularity songs={this.state.songs} />;
+            }}
+          />
+          <Route
+            path="/songs/byGenre"
+            render={() => {
+              return <SongsByGenre songs={this.state.songs} />;
+            }}
+          />
+          <Route
+            exact
+            path="/songs/:id"
+            render={() => {
+              return <SingleSong songs={this.state.songs} />;
+            }}
+          />
+        </Switch>
+      </>
     );
   }
 }
