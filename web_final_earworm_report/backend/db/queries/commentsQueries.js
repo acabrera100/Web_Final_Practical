2a8b50e6fcc801b6e45c7adf6fc3855840332b70
Andres.C <andrescabrera@pursuit.org>
2019-03-18 14:35:42 -0400

    switched put request to patch request

--- web_final_earworm_report/backend/db/queries/commentsQueries.js
+++ web_final_earworm_report/backend/db/queries/commentsQueries.js
@@ -51,8 +51,52 @@ const createComment = (req, res, next) => {
       return next(err);
     });
 };
+// PUT REQUEST
+
+// const updateComment = (req, res, next) => {
+//   db.none(
+//     'UPDATE comments SET comment_body = ${comment_body},user_id=${user_id}' +
+//       ', song_id=${song_id} WHERE id=${id}',
+//     {
+//
+//       comment_body: req.body.comment_body,
+//       user_id: parseInt(req.body.user_id),
+//       song_id: parseInt(req.body.song_id),
+//       id: parseInt(req.params.id)
+//     })
+//     .then(() => {
+//       res.status(200).json({
+//         status: "success",
+//         message: "You Updated A Comment"
+//       });
+//     })
+//     .catch(err => {
+//       return next(err);
+//     });
+// };
+// PATCH REQEUST (partial update)
+const updateComment = (req, res, next) => {
+  db.none(
+    "UPDATE comments SET user_id=${user_id}, comment_body=${comment_body} WHERE id=${id}",
+    {
+      user_id: parseInt(req.body.user_id),
+      comment_body: req.body.comment_body,
+      id: parseInt(req.params.id)
+    }
+  )
+    .then(() => {
+      res.status(200).json({
+        status: "success",
+        message: "You Updated a Comment in the Database!"
+      });
+    })
+    .catch(err => {
+      return next(err);
+    });
+};
 module.exports = {
   getAllComments,
   getAllCommentsForSpecificSong,
-  createComment
+  createComment,
+  updateComment
 };
--- web_final_earworm_report/backend/routes/comments.js
+++ web_final_earworm_report/backend/routes/comments.js
@@ -4,10 +4,12 @@ var router = express.Router();
 const {
   getAllComments,
   getAllCommentsForSpecificSong,
-  createComment
+  createComment,
+  updateComment
 } = require("../db/queries/commentsQueries.js");
 
 router.get("/", getAllComments);
 router.get("/bySong/:id", getAllCommentsForSpecificSong);
 router.post("/",createComment);
+router.patch("/:id",updateComment)
 module.exports = router;
